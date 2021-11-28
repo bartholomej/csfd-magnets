ab11dfb7364a5d657e7d94bc24aa3df7ed210e25
BART! <bart@bartweb.cz>
2021-11-28 11:58:00 +0100

    feat(providers): more providers. #36

--- src/background.ts
+++ src/background.ts
@@ -1,23 +1,33 @@
 import { piratebay } from 'piratebay-scraper';
+import { TPBProvider } from 'piratebay-scraper/interfaces';
+
+const PROVIDERS = ['https://tpb.party', 'https://thepiratebay.zone/', 'https://pirateproxy.live/'];
+
+let index = 0;
 
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (request.contentScriptQuery === 'fetchData') {
-    piratebay
-      .search(request.searchQuery)
-      .then((response) => {
-        if (response) {
-          return response;
-        }
-        throw new Error("Can't connect to movie provider :(");
-      })
-      .then((response) => {
-        sendResponse(response);
-      })
-      .catch((error) => {
-        throw new Error(error);
-      });
+    getData(request, sender, sendResponse, PROVIDERS[index]);
     return true;
   } else {
     return false;
   }
 });
+
+const getData = (request: any, sender: any, sendResponse: any, provider: TPBProvider) => {
+  piratebay
+    .search(request.searchQuery, provider)
+    .then((response) => {
+      if (response) {
+        return response;
+      }
+      throw new Error("Can't connect to movie provider :(");
+    })
+    .then((response) => {
+      sendResponse(response);
+    })
+    .catch((error) => {
+      index = index + 1;
+      getData(request, sender, sendResponse, PROVIDERS[index]);
+    });
+};
